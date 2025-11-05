412066abcc914431d715ae96f7b134bfad14ea7a
xdream oldlu <xdream@gmail.com>
2025-11-05 10:50:33 +0800

    add clerk and development docs

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+	return (
+		<div className='flex items-center justify-center text-xs border-t mt-2 p-2'>
+			<div>made with love by <a href="XXXXXXXXXXXXXXXXXXXXXXXXXXX" className="underline">@0x0x0x0x</a></div>
+		</div>
+	)
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
+// import { NavSearch } from './navSearch'
+import { Button } from './ui/button'
+import Link from 'next/link'
+
+export const Navbar = () => {
+	return (
+		<nav className="sticky top-0 left-0 w-full h-12 flex items-center justify-between gap-2 bg-foreground/20 backdrop-blur-md p-2">
+			<div className="font-bold">
+				<Link href="/">LEwS</Link>
+			</div>
+			<div className="flex items-center">
+				{/* <NavSearch /> */}
+				<SignedIn>
+					<UserButton appearance={{ elements: { avatarBox: 'ml-3' } }} />
+				</SignedIn>
+				<SignedOut>
+					<SignInButton mode="modal">
+						<Button variant="link">登入</Button>
+					</SignInButton>
+				</SignedOut>
+			</div>
+		</nav>
+	)
+}
